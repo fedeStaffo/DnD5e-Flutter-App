6d395b59b498ddcf6d97cac6ea2cdccbc07a1ccf
Federico Staffolani <S1097620@studenti.univpm.it>
2023-06-01 17:45:00 +0200

    Initial commit

---
+++ android/app/src/main/kotlin/com/progettodd/progetto_dd/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.progettodd.progetto_dd
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,125 @@
+import 'package:flutter/material.dart';
+import 'package:progetto_dd/view/info.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Scheda D&D 5e',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSwatch(
+          primarySwatch: Colors.blue,
+          // PROVA: Prova a cambiare il primarySwatch con un altro colore, come Colors.green.
+        ),
+      ),
+      home: const MyHomePage(title: 'Scheda D&D 5e'),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.secondary,
+        title: Text(title),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              decoration: const BoxDecoration(
+                color: Colors.blue,
+              ),
+              child: Image.asset(
+                'assets/images/drawer_image.jpg',
+                fit: BoxFit.fill,
+              ),
+            ),
+            ListTile(
+              leading: const Icon(Icons.person),
+              title: const Text('Profilo'),
+              onTap: () {
+                // Azioni da eseguire quando "Profilo" viene selezionato
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.logout),
+              title: const Text('Logout'),
+              onTap: () {
+                // Azioni da eseguire quando "Logout" viene selezionato
+              },
+            ),
+            const Divider(color: Colors.black,),
+            ListTile(
+              leading: const Icon(Icons.info),
+              title: const Text('Info e contatti'),
+              onTap: () {
+                // Azioni da eseguire quando "Info e contatti" viene selezionato
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const InfoScreen()),
+                  );
+              },
+            ),
+            const Divider(color: Colors.black,),
+            ListTile(
+              leading: const Icon(Icons.close),
+              title: const Text('Chiudi'),
+              onTap: () => Navigator.pop(context),
+            )
+          ],
+        ),
+      ),
+      body: Container(
+        decoration: BoxDecoration(
+          color: Colors.lightBlue.withOpacity(0.5),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              SizedBox(
+                width: 200,
+                child: Image.asset('assets/images/knight.png'),
+              ),
+              const SizedBox(height: 16),
+              const Text(
+                'Personaggi',
+                style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black),
+              ),
+              const SizedBox(height: 32),
+              SizedBox(
+                width: 200,
+                child: Image.asset('assets/images/treasure_map.png'),
+              ),
+              const SizedBox(height: 16),
+              const Text(
+                'Campagne',
+                style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
